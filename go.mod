3b36a54c981241006d9e77918a0e489a83e6753f
vuthdev <serey7vuth@gmail.com>
2026-07-08 09:48:25 +0700

    feat: add khqr png generator

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module khqr-learn
 
 go 1.22
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e // indirect
+)
--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 
 	"khqr-learn/internal/bakong"
 	"khqr-learn/internal/khqr"
 
 	"github.com/joho/godotenv"
+	"github.com/skip2/go-qrcode"
 )
 
 func main() {
@@ -17,49 +19,78 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	qr, err := khqr.GenerateIndividual(khqr.IndividualInfo{
-		BakongAccountID: "sereyvuth_duong@bkrt",
-		MerchantName:    "Sereyvuth Duong",
-		MerchantCity:    "Phnom Penh",
-		Amount:          1.00,
-		Currency:        "USD",
-		//BillNumber:      "TEST001",
-	})
+	outputFolder := "qrcodes"
+	fileName := "my_khqr_invoice.png"
+
+	err = os.Mkdir(outputFolder, os.ModePerm) 
 	if err != nil {
-		fmt.Println("generate error:", err)
-		return
+		log.Fatalf("Failed to create folder: %v", err)
 	}
 
-	merchantQr, err := khqr.GenerateMerchant(
-		khqr.MerchantInfo{
-			AccountID: "sereyvuth_duong@bkrt",
-			MerchantName:    "Sereyvuth Duong",
-			MerchantCity:    "Phnom Penh",
-			Amount:          1.00,
-			Currency:        "USD",
-			BillNumber:      "TEST001",
-			MerchantID: "TESTMERCHANTID1",
-			AcquiringBank: "aba",
-		})
+	filePath := filepath.Join(outputFolder, fileName)
 
-	fmt.Println("QR:", merchantQr)
+	// merchantQr, err := khqr.GenerateMerchant(
+	// 	khqr.MerchantInfo{
+	// 		AccountID: "sereyvuth_duong@bkrt",
+	// 		MerchantName:    "Sereyvuth Duong",
+	// 		MerchantCity:    "Phnom Penh",
+	// 		Amount:          1.00,
+	// 		Currency:        "USD",
+	// 		BillNumber:      "TEST001",
+	// 		MerchantID: "TESTMERCHANTID1",
+	// 		AcquiringBank: "aba",
+	// 	})
 
-	hashIndi := khqr.HashMD5(qr)
-	hashMerc := khqr.HashMD5(merchantQr)
-	fmt.Println("MD5 for Individual:", hashIndi)
-	fmt.Println("MD5 for Merchant:", hashMerc)
+	// fmt.Println("QR:", merchantQr)
+
+	// hashMerc := khqr.HashMD5(merchantQr)
+	
+	// fmt.Println("MD5 for Merchant:", hashMerc)
 
 	var token string = os.Getenv("BAKONG_TOKEN")
 
+	// example data
+	var accountID string = "sereyvuth_duong@test"
+	var merchantName string = "Sereyvuth Duong"
+	var merchantCity string = "Phnom Penh"
+	var amount float64 = 1.00
+	var currency string = "USD"
+
 	client := bakong.NewClient(token)
-	check_account, err := client.CheckAccountByID("sereyvuth_duong@bkrt")
+	check_account, err := client.CheckAccountByID(accountID)
 	if err != nil {
 		fmt.Printf("Error trying to check account: %v\n", err)
 		return
 	}
 	if check_account {
-		fmt.Printf("Account Id Exist")
+		qr, err := khqr.GenerateIndividual(khqr.IndividualInfo{
+			BakongAccountID: accountID,
+			MerchantName:    merchantName,
+			MerchantCity:    merchantCity,
+			Amount:          amount,
+			Currency:        currency,
+			//BillNumber:      "TEST001",
+		})
+		if err != nil {
+			fmt.Println("generate error:", err)
+			return
+		}
+
+		fmt.Println("KHQR string: ", qr)
+		hashIndi := khqr.HashMD5(qr)
+		fmt.Println("MD5 for Individual:", hashIndi)
+		fmt.Printf("Account Exist")
+
+		fmt.Println("generating QR code...")
+		pngBytes, _ := qrcode.Encode(qr, qrcode.Medium, 256)
+
+		err = os.WriteFile(filePath, pngBytes, 0644)
+		if err != nil {
+			log.Fatalf("Failed to save the PNG file: %v", err)
+		}
+
+		fmt.Printf("🎉 Success! Your KHQR image has been saved to: %s\n", filePath)
 	} else {
-		fmt.Println("Account ID not exist")
+		fmt.Println("Account not exist")
 	}
 }
